9c1ff13c2e6deac4be684aaf70de4fa0b7aa287f
optclblast <molodoy.p4elovek@yandex.ru>
2024-06-01 18:12:35 +0300

    lint fix

--- internal/usecase/delta_test.go
+++ internal/usecase/delta_test.go
@@ -67,9 +67,11 @@ var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 
 func RandStringRunes(n int) string {
 	b := make([]rune, n)
+
 	for i := range b {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
+
 	return string(b)
 }
 
